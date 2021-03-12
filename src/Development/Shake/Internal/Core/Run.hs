398f81f58c85628b1b3df82c65c2ab668f805ed2
Pepe Iborra <pepeiborra@gmail.com>
2021-03-12 16:48:31 +0000

    Force show timings

--- src/Development/Shake/Internal/Core/Run.hs
+++ src/Development/Shake/Internal/Core/Run.hs
@@ -112,8 +112,10 @@ run RunState{..} oneshot actions2 =
 
         res <- withCleanup $ \cleanup -> do
             register cleanup $ do
-                when (shakeTimings && shakeVerbosity >= Info) $
-                    writeIORef timingsToShow . Just =<< getTimings
+                when (shakeTimings && shakeVerbosity >= Info) $ do
+                    timings <- getTimings
+                    writeIORef timingsToShow $ Just timings
+                    mapM_ putStrLn timings
                 resetTimings
 
             start <- offsetTime
